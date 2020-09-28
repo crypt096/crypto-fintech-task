d2e200683679161a4e298a8d87f01778617a1e96
crypt0 <acvjetan@gmail.com>
2020-09-28 20:05:32 +0200

    Add initial content to Navbar component.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        ></button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="/">
+            Navbar
+          </a>
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Link
+              </a>
+            </li>
+          </ul>
+          <button className="btn btn-primary">This is just a button</button>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;

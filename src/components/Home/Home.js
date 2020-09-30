7545e2f5bceefd6ef19b4b5c8ade755d78ad3e44
crypt0 <acvjetan@gmail.com>
2020-09-30 11:07:05 +0200

    Implemented basic Web Socket lifecycle.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 function Home() {
+  let bitfinexSocket = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
+
+  bitfinexSocket.onmessage = (msg) => {
+    console.log(msg.data);
+  };
+
+  let msg = JSON.stringify({
+    event: "subscribe",
+    channel: "ticker",
+    symbol: "tBTCUSD",
+  });
+
+  bitfinexSocket.onopen = () => bitfinexSocket.send(msg);
   return <div>Hello from Home component</div>;
 }
 
